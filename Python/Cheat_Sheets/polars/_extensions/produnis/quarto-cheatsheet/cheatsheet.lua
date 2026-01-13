9023489bc2af564d1a62cf37a7dc855ae1319917
esuriddick <77727062+esuriddick@users.noreply.github.com>
2026-01-13 00:16:39 +0000

    Updated polars' cheat sheet

--- Python/Cheat_Sheets/polars/_extensions/produnis/quarto-cheatsheet/cheatsheet.lua
+++ Python/Cheat_Sheets/polars/_extensions/produnis/quarto-cheatsheet/cheatsheet.lua
@@ -1,7 +1,7 @@
 function generateCheatBlockLatex(block)
     local title = pandoc.utils.stringify(block.attributes["title"])
     
-    -- create a small Pandoc-Dokument with block.content
+    -- create a small Pandoc-Document with block.content
     local pandocDocument = pandoc.Pandoc(block.content, {})
 
     -- use pandoc.write to convert into LaTeX
@@ -9,11 +9,10 @@ function generateCheatBlockLatex(block)
 
     local latexCode = "\\begin{tikzpicture}\n"
     latexCode = latexCode .. "    \\node [mybox] (box){%\n"
-    latexCode = latexCode .. "     " .. latexContent .. "\n"
-    latexCode = latexCode .. "    };\n"
-    latexCode = latexCode .. "    %------------ Neues Semester Header ---------------------\n"
+    latexCode = latexCode .. "        " .. latexContent .. "};\n"
     latexCode = latexCode .. "    \\node[fancytitle, right=10pt] at (box.north west) {" .. title .. "};\n"
     latexCode = latexCode .. " \\end{tikzpicture}"
+    latexCode = latexCode .. "\\vspace{5pt}"
 
     return pandoc.RawBlock('latex', latexCode)
 end
